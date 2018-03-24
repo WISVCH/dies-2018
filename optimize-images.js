c99ff7eadfacacb67717faa76c70be9fcce70b9c
Tim van der Lippe <tvanderlippe@gmail.com>
2018-03-24 13:19:42 +0100

    Reduce duplication in photo-gallery-show-logic

--- optimize-images.js
+++ optimize-images.js
@@ -7,7 +7,8 @@ const root = path.resolve(process.cwd(), 'images');
 const optimizedImagesRoot = path.resolve(process.cwd(), 'images-optimized');
 const imageOptions = {
   committee: '200x200',
-  ['pre-pre-dies']: '250,scale-down'
+  ['pre-pre-dies']: '250,scale-down',
+  ['pre-dies']: '250,scale-down'
 };
 
 // Optimize images with ImageOptim
