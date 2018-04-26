3dda22b6a21af8b931e4bec14c6819d87ecf0b16
Jeroen <jeroentjuuhh@gmail.com>
2018-04-26 16:05:02 +0200

    added the optimized image path

--- create-image-mapping.js
+++ create-image-mapping.js
@@ -8,14 +8,13 @@ const exclude_folders = ["committee"];
 
 for (const imageFile of glob.sync('**/*', {cwd})) {
   const day = path.dirname(imageFile);
-
+  
   if (day === '.' || exclude_folders.indexOf(day) > -1) {
     continue;
   }
-  
+
   images[day] = images[day] || [];
-  
-  images[day].push(imageFile);
+  images[day].push("images-optimized" + path.sep + imageFile);
 }
 
 fs.writeFileSync('image-map.json', JSON.stringify(images));
